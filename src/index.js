d9c7ec870932273fe74bdce6984d7c43952be802
Jon Patterson <JonPatt92@gmail.com>
2020-02-20 10:45:14 -0700

    Call wordService to debug and see output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import $ from 'jquery'
+const wordService = require('wordService')
 
 $(document).ready(() => {
   // have fun!
 })
+
+let favWord = await wordService.getWord;
+console.log(favWord);
